9f079e3e36d5176319c295a53741e815d707fe93
Michael Holzworth <holzworth.19@osu.edu>
2024-04-05 22:50:24 -0400

    end of video 1

--- Green.General.API-main-main/Project.Green.General.API/Controllers/CatalogController.cs
+++ Green.General.API-main-main/Project.Green.General.API/Controllers/CatalogController.cs
@@ -10,8 +10,19 @@ namespace Project.Green.General.Api.Controllers {
 
         public IActionResult GetItems(){
 
-            return Ok("Hello World!");
+            var items = new List<Item>(){
+                new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m),
+                new Item("Shorts", "Ohio State Shorts", "Nike", 44.99m)
+            };
+            return Ok(items);
+        }
 
+        [HttpGet("{id:int}")]
+
+        public IActionResult GetItem(int id) {
+            var item = new Item("Shirt", "Ohio State Shirt", "Nike", 29.99m);
+            item.Id = id;
+            return Ok(item);
         }
     }
 }
